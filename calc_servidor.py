8a7186c164a11ccfff1a147fb0217510ca48f832
Joallison Avelino <joallison_jose@hotmail.com>
2017-06-22 14:15:48 -0300

    Implementação do cálculo raiz quadrada.

--- calc_servidor.py
+++ calc_servidor.py
@@ -19,20 +19,24 @@ while True:
 	escolha, clientAddress = serverSocket.recvfrom(2048)
 	a = escolha.decode('utf-8')
 	print (a)
-
-	num1, clientAddress = serverSocket.recvfrom(2048)
-	b1 = num1.decode('utf-8')
-	print (b1)
-
-	num2, clientAddress = serverSocket.recvfrom(2048)
-	b2 = num2.decode('utf-8')
-	print (b2)
-
+	
 	if a == '1':
+		num1, clientAddress = serverSocket.recvfrom(2048)
+		b1 = num1.decode('utf-8')
+		print (b1)
+
+		num2, clientAddress = serverSocket.recvfrom(2048)
+		b2 = num2.decode('utf-8')
+		print (b2)
+		
 		Message = soma(int(b1), int(b2))
 		modifiedMessage = str(Message).encode('utf-8')
 		serverSocket.sendto(modifiedMessage, clientAddress)  
 	elif a == '2':
+		num1, clientAddress = serverSocket.recvfrom(2048)
+		b1 = num1.decode('utf-8')
+		print (b1)
+		
 		Message = raiz(int(b1))
 		print (Message)
 		modifiedMessage = str(Message).encode('utf-8')
